3bbd923dc74ba70ffff9dfab91dc17b572f6e386
Jeff May <jeff.n.may@gmail.com>
2020-09-22 14:27:46 -0700

    Add and / or infix operators

--- core/src/main/scala/com/rallyhealth/vapors/core/dsl/LogicalOps.scala
+++ core/src/main/scala/com/rallyhealth/vapors/core/dsl/LogicalOps.scala
@@ -1,10 +1,27 @@
 package com.rallyhealth.vapors.core.dsl
 
 import com.rallyhealth.vapors.core.data.{Intersect, Union}
+import com.rallyhealth.vapors.core.dsl
 
 final class LogicalOps[T, A](private val exp: AnyExp[T, A]) extends AnyVal {
 
-  def &&(o: AnyExp[T, A])(implicit A: Intersect[A]): AnyExp[T, A] = and(exp, o)
+  /**
+    * Builds an AND expression using infix notation.
+    */
+  def and(o: AnyExp[T, A])(implicit A: Intersect[A]): AnyExp[T, A] = dsl.and(exp, o)
 
-  def ||(o: AnyExp[T, A])(implicit A: Union[A]): AnyExp[T, A] = or(exp, o)
+  /**
+    * Same as [[and]] but with higher precedent.
+    */
+  def &&(o: AnyExp[T, A])(implicit A: Intersect[A]): AnyExp[T, A] = dsl.and(exp, o)
+
+  /**
+    * Builds an OR expression using infix notation.
+    */
+  def or(o: AnyExp[T, A])(implicit A: Union[A]): AnyExp[T, A] = dsl.or(exp, o)
+
+  /**
+    * Same as [[or]] but with higher precedent.
+    */
+  def ||(o: AnyExp[T, A])(implicit A: Union[A]): AnyExp[T, A] = dsl.or(exp, o)
 }
