6a0533ee577642083fe2ba8fe452897977a27b4c
Jeff May <jeff.n.may@gmail.com>
2020-09-29 01:10:31 -0700

    Add WindowSpec
    - Add unit tests for Window methods across multiple types
    - Update scalafmt to allow min, min infix notation
    - Reuse some Window methods
    - Add scalatestplus-scalacheck and scalacheck-ops to the project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,12 +7,16 @@ object Dependencies {
   private final val catsVersion = "2.1.1"
   private final val kindProjectorVersion = "0.11.0"
   private final val scalaCheckVersion = "1.14.3"
+  private final val scalaCheckOpsVersion = "2.5.0"
   private final val scalaTestVersion = "3.2.2"
+  private final val scalaTestPlusScalaCheckVersion = "3.1.4.0"
 
   private val catsCore = "org.typelevel" %% "cats-core" % catsVersion
   private val catsFree = "org.typelevel" %% "cats-free" % catsVersion
   private val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckVersion
+  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-14" % scalaCheckOpsVersion
   private val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+  private val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-14" % scalaTestPlusScalaCheckVersion
   private def scalaReflect(scalacVersion: String): ModuleID = "org.scala-lang" % "scala-reflect" % scalacVersion
 
   final object Plugins {
@@ -33,7 +37,9 @@ object Dependencies {
       ) ++ Seq(
         // Test-only dependencies
         scalaCheck,
+        scalaCheckOps,
         scalaTest,
+        scalaTestPlusScalaCheck,
       ).map(_ % Test)
   }
 
