a32258a0e6274fb8f1cd40aa9c8b9075bb5552db
Jeff May <jeff.n.may@gmail.com>
2020-10-28 11:55:58 -0700

    Add deprecation warnings for old where builder formats

--- core/src/main/scala/com/rallyhealth/vapors/factfilter/dsl/WhereExpBuilder.scala
+++ core/src/main/scala/com/rallyhealth/vapors/factfilter/dsl/WhereExpBuilder.scala
@@ -122,49 +122,53 @@ class WhereBuilder[T, U <: T : ClassTag : TypeTag] private[dsl] (factTypeSet: Fa
     collectFacts(exp)
   }
 
-//  def whereAnyFact(buildExp: CondBuilder[TypedFact[U], TypedFact[U]] => CondExp[TypedFact[U]]): TerminalFactsExp = {
-//    collectFacts {
-//      FreeApplicative.lift {
-//        whereAny(TypedFact.lens[U], buildExp)
-//      }
-//    }
-//  }
-//
-//  def whereAnyFactValue(buildExp: CondBuilder[TypedFact[U], U] => CondExp[TypedFact[U]]): TerminalFactsExp = {
-//    collectFacts {
-//      FreeApplicative.lift {
-//        whereAny(TypedFact.value[U], buildExp)
-//      }
-//    }
-//  }
-//
-//  private def whereAny[V](
-//    lens: NamedLens[TypedFact[U], V],
-//    buildExp: CondBuilder[TypedFact[U], V] => CondExp[TypedFact[U]],
-//  ): ExpAlg[List[TypedFact[U]], TypedResultSet[U]] = {
-//    ExpAlg.Exists[List[TypedFact[U]], TypedFact[U], TypedResultSet[U]](
-//      _.toList,
-//      buildExp(new CondBuilder(lens)),
-//      TypedResultSet.fromNel,
-//      _ => NoFactsMatch(),
-//    )
-//  }
-//
-//  def whereEveryFact(buildExp: CondBuilder[TypedFact[U], TypedFact[U]] => CondExp[TypedFact[U]]): TerminalFactsExp = {
-//    collectFacts {
-//      FreeApplicative.lift {
-//        whereEvery(TypedFact.lens[U], buildExp)
-//      }
-//    }
-//  }
-//
-//  def whereEveryFactValue(buildExp: CondBuilder[TypedFact[U], U] => CondExp[TypedFact[U]]): TerminalFactsExp = {
-//    collectFacts {
-//      FreeApplicative.lift {
-//        whereEvery(TypedFact.value[U], buildExp)
-//      }
-//    }
-//  }
+  @deprecated("Use where(_.value.exists(...)) instead.", "0.2.0")
+  def whereAnyFact(buildExp: CondBuilder[TypedFact[U], TypedFact[U]] => CondExp[TypedFact[U]]): TerminalFactsExp = {
+    collectFacts {
+      FreeApplicative.lift {
+        whereAny(TypedFact.lens[U], buildExp)
+      }
+    }
+  }
+
+  @deprecated("Use where(_.exists(...)) instead.", "0.2.0")
+  def whereAnyFactValue(buildExp: CondBuilder[TypedFact[U], U] => CondExp[TypedFact[U]]): TerminalFactsExp = {
+    collectFacts {
+      FreeApplicative.lift {
+        whereAny(TypedFact.value[U], buildExp)
+      }
+    }
+  }
+
+  private def whereAny[V](
+    lens: NamedLens[TypedFact[U], V],
+    buildExp: CondBuilder[TypedFact[U], V] => CondExp[TypedFact[U]],
+  ): ExpAlg[List[TypedFact[U]], TypedResultSet[U]] = {
+    ExpAlg.Exists[List[TypedFact[U]], TypedFact[U], TypedResultSet[U]](
+      _.toList,
+      buildExp(new CondBuilder(lens)),
+      TypedResultSet.fromNel,
+      _ => NoFactsMatch(),
+    )
+  }
+
+  @deprecated("Use where(_.forall(...)) instead.", "0.2.0")
+  def whereEveryFact(buildExp: CondBuilder[TypedFact[U], TypedFact[U]] => CondExp[TypedFact[U]]): TerminalFactsExp = {
+    collectFacts {
+      FreeApplicative.lift {
+        whereEvery(TypedFact.lens[U], buildExp)
+      }
+    }
+  }
+
+  @deprecated("Use where(_.value.forall(...)) instead.", "0.2.0")
+  def whereEveryFactValue(buildExp: CondBuilder[TypedFact[U], U] => CondExp[TypedFact[U]]): TerminalFactsExp = {
+    collectFacts {
+      FreeApplicative.lift {
+        whereEvery(TypedFact.value[U], buildExp)
+      }
+    }
+  }
 
   private def collectFacts(exp: Exp[List[TypedFact[U]], TypedResultSet[U]]): TerminalFactsExp = {
     FreeApplicative.lift {
@@ -180,15 +184,15 @@ class WhereBuilder[T, U <: T : ClassTag : TypeTag] private[dsl] (factTypeSet: Fa
     }
   }
 
-//  private def whereEvery[V](
-//    lens: NamedLens[TypedFact[U], V],
-//    buildExp: CondBuilder[TypedFact[U], V] => CondExp[TypedFact[U]],
-//  ): ExpAlg[List[TypedFact[U]], TypedResultSet[U]] = {
-//    ExpAlg.ForAll[List[TypedFact[U]], TypedFact[U], TypedResultSet[U]](
-//      _.toList,
-//      buildExp(new CondBuilder(lens)),
-//      _ => NoFactsMatch(),
-//      TypedResultSet(_),
-//    )
-//  }
+  private def whereEvery[V](
+    lens: NamedLens[TypedFact[U], V],
+    buildExp: CondBuilder[TypedFact[U], V] => CondExp[TypedFact[U]],
+  ): ExpAlg[List[TypedFact[U]], TypedResultSet[U]] = {
+    ExpAlg.ForAll[List[TypedFact[U]], TypedFact[U], TypedResultSet[U]](
+      _.toList,
+      buildExp(new CondBuilder(lens)),
+      _ => NoFactsMatch(),
+      TypedResultSet(_),
+    )
+  }
 }
