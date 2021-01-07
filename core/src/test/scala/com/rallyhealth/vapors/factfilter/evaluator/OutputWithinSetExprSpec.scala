b47cc52fefb53d7979e9e32231e1cc765cec58fb
Jeff May <jeff.n.may@gmail.com>
2021-01-07 09:33:34 -0800

    Add unit tests

---
+++ core/src/test/scala/com/rallyhealth/vapors/factfilter/evaluator/OutputWithinSetExprSpec.scala
@@ -0,0 +1,35 @@
+package com.rallyhealth.vapors.factfilter.evaluator
+
+import com.rallyhealth.vapors.factfilter.Example.{FactTypes, JoeSchmoe}
+import com.rallyhealth.vapors.factfilter.data.{Evidence, FactSet}
+import com.rallyhealth.vapors.factfilter.dsl.CaptureP.unit._
+import org.scalatest.wordspec.AnyWordSpec
+import com.rallyhealth.vapors.factfilter.dsl.ExprDsl._
+
+class OutputWithinSetExprSpec extends AnyWordSpec {
+
+  "Expr.OutputWithinSet" should {
+
+    "find an asthma tag in a set that contains it" in {
+      val q = withFactsOfType(FactTypes.Tag).where {
+        _.exists {
+          _.value.in(Set("asthma", "diabetes"))
+        }
+      }
+      val result = eval(JoeSchmoe.factTable)(q)
+      assert(result.output.value)
+      assertResult(Evidence(JoeSchmoe.asthmaTag))(result.output.evidence)
+    }
+
+    "not find an asthma tag in a set that does not contain it" in {
+      val q = withFactsOfType(FactTypes.Tag).where {
+        _.exists {
+          _.value.in(Set("diabetes"))
+        }
+      }
+      val result = eval(JoeSchmoe.factTable)(q)
+      assert(!result.output.value)
+      assertResult(Evidence.none)(result.output.evidence)
+    }
+  }
+}
