2d7fcd6b98e1bf32a9cfac4b95ae1bc4eddd6d81
Jeff May <jeff.n.may@gmail.com>
2021-01-19 22:40:10 -0800

    Chain lenses on consecutive calls to .get() on ValExprBuilder

---
+++ core/src/test/scala/com/rallyhealth/vapors/factfilter/dsl/ExprBuilderSpec.scala
@@ -0,0 +1,60 @@
+package com.rallyhealth.vapors.factfilter.dsl
+
+import com.rallyhealth.vapors.core.algebra.Expr
+import com.rallyhealth.vapors.core.data.DataPath
+import com.rallyhealth.vapors.factfilter.Example.{FactTypes, GenericMeasurement, Probs}
+import com.rallyhealth.vapors.factfilter.dsl.ExprDsl._
+import org.scalatest.Inside.inside
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.time.Instant
+
+class ExprBuilderSpec extends AnyWordSpec {
+
+  "ValExprBuilder" should {
+
+    "combine lenses from chained .get() methods" in {
+      val q = withFactsOfType(FactTypes.GenericMeasurement).where {
+        _.exists {
+          _.get(_.select(_.value)).get(_.select(_.value)) > 0.0
+        }
+      }
+      inside(q) {
+        case Expr.WithFactsOfType(_, Expr.ExistsInOutput(_, condExpr, _), _) =>
+          inside(condExpr) {
+            case Expr.OutputWithinWindow(Expr.SelectFromOutput(_, valueLens, _), _, _) =>
+              assertResult(DataPath.empty.atField("value").atField("value")) {
+                valueLens.path
+              }
+              val fact = FactTypes.GenericMeasurement(GenericMeasurement("example", 1.0, "m", Instant.now()))
+              assertResult(fact.value.value) {
+                valueLens.get(fact)
+              }
+          }
+      }
+    }
+
+    "combine lenses from .get() and .getFoldable() methods" in {
+      val q = withFactsOfType(FactTypes.ProbabilityToUse).where {
+        _.exists {
+          _.get(_.select(_.value)).getFoldable(_.select(_.scores)).isEmpty
+        }
+      }
+      inside(q) {
+        case Expr.WithFactsOfType(_, Expr.ExistsInOutput(_, condExpr, _), _) =>
+          inside(condExpr) {
+            case Expr.OutputIsEmpty(Expr.SelectFromOutput(_, valueLens, _), _) =>
+              assertResult(DataPath.empty.atField("value").atField("scores")) {
+                valueLens.path
+              }
+              val fact = FactTypes.ProbabilityToUse(Probs(Map()))
+              assertResult(fact.value.scores) {
+                valueLens.get(fact)
+              }
+          }
+      }
+    }
+  }
+
+}
