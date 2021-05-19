73637888bd67efe2e1cdd0e7b6e32817a5dfa034
Jeff May <jeff.n.may@gmail.com>
2021-05-19 15:59:17 -0700

    Bump all versions to the latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,23 +2,23 @@ import sbt._
 
 object Dependencies {
 
-  final val Scala_2_13 = "2.13.0"
+  final val Scala_2_13 = "2.13.6"
 
-  private final val catsVersion = "2.3.0"
-  private final val kindProjectorVersion = "0.11.0"
-  private final val scalaCheckVersion = "1.15.1"
-  private final val scalaCheckOpsVersion = "2.5.1"
-  private final val scalaTestVersion = "3.2.2"
-  private final val scalaTestPlusScalaCheckVersion = "3.1.4.0"
-  private final val shapelessVersion = "2.3.3"
+  private final val catsVersion = "2.6.1"
+  private final val kindProjectorVersion = "0.13.0"
+  private final val scalaCheckVersion = "1.15.4"
+  private final val scalaCheckOpsVersion = "2.6.0"
+  private final val scalaTestVersion = "3.2.9"
+  private final val scalaTestPlusScalaCheckVersion = "3.2.9.0"
+  private final val shapelessVersion = "2.3.7"
 
   private val alleyCatsCore = "org.typelevel" %% "alleycats-core" % catsVersion
   private val catsCore = "org.typelevel" %% "cats-core" % catsVersion
   private val catsFree = "org.typelevel" %% "cats-free" % catsVersion
   private val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckVersion
-  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-14" % scalaCheckOpsVersion
+  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-15" % scalaCheckOpsVersion
   private val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
-  private val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-14" % scalaTestPlusScalaCheckVersion
+  private val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % scalaTestPlusScalaCheckVersion
   private def scalaReflect(scalacVersion: String): ModuleID = "org.scala-lang" % "scala-reflect" % scalacVersion
   private val shapeless = "com.chuusai" %% "shapeless" % shapelessVersion
 
