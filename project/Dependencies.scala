d0d6c08dac8a3c904a320a1f11c3c4016d13bc67
Jeff May <jeff.n.may@gmail.com>
2021-08-11 22:07:43 -0700

    Upgrade scalacheck-ops

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   private val catsCore = "org.typelevel" %% "cats-core" % "2.6.1"
   private val catsFree = "org.typelevel" %% "cats-free" % "2.6.1"
   private val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.4"
-  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-15" % "2.6.0"
+  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-15" % "2.7.1"
   private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
   private val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % "3.2.9.0"
   private def scalaReflect(scalacVersion: String): ModuleID = "org.scala-lang" % "scala-reflect" % scalacVersion
