2a4a91bf1b180dbf79ba73aac2735bf53e3da06f
Jeff May <jeff.n.may@gmail.com>
2021-08-11 22:04:00 -0700

    Inline dependencies versions for better IDE support

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,28 +4,20 @@ object Dependencies {
 
   final val Scala_2_13 = "2.13.6"
 
-  private final val catsVersion = "2.6.1"
-  private final val kindProjectorVersion = "0.13.0"
-  private final val scalaCheckVersion = "1.15.4"
-  private final val scalaCheckOpsVersion = "2.6.0"
-  private final val scalaTestVersion = "3.2.9"
-  private final val scalaTestPlusScalaCheckVersion = "3.2.9.0"
-  private final val shapelessVersion = "2.3.7"
-
-  private val alleyCatsCore = "org.typelevel" %% "alleycats-core" % catsVersion
-  private val catsCore = "org.typelevel" %% "cats-core" % catsVersion
-  private val catsFree = "org.typelevel" %% "cats-free" % catsVersion
-  private val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckVersion
-  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-15" % scalaCheckOpsVersion
-  private val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
-  private val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % scalaTestPlusScalaCheckVersion
+  private val alleyCatsCore = "org.typelevel" %% "alleycats-core" % "2.6.1"
+  private val catsCore = "org.typelevel" %% "cats-core" % "2.6.1"
+  private val catsFree = "org.typelevel" %% "cats-free" % "2.6.1"
+  private val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.4"
+  private val scalaCheckOps = "com.rallyhealth" %% "scalacheck-ops_1-15" % "2.6.0"
+  private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
+  private val scalaTestPlusScalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % "3.2.9.0"
   private def scalaReflect(scalacVersion: String): ModuleID = "org.scala-lang" % "scala-reflect" % scalacVersion
-  private val shapeless = "com.chuusai" %% "shapeless" % shapelessVersion
+  private val shapeless = "com.chuusai" %% "shapeless" % "2.3.7"
 
   final object Plugins {
 
     val kindProjector = {
-      compilerPlugin("org.typelevel" %% "kind-projector" % kindProjectorVersion).cross(CrossVersion.full)
+      compilerPlugin("org.typelevel" %% "kind-projector" % "0.13.0").cross(CrossVersion.full)
     }
   }
 
