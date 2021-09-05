3566740677919071e187a0d6a53f26315494fdf3
Jeff May <jeff.n.may@gmail.com>
2021-09-05 10:20:30 -0700

    Support serializing expression results with Circe

--- core/src/main/scala/vapors/v1/dsl/circe/circe.scala
+++ core/src/main/scala/vapors/v1/dsl/circe/circe.scala
@@ -5,12 +5,17 @@ package vapors.v1.dsl
 import vapors.v1.algebra.ExprResult
 import vapors.v1.engine.InterpretExprResultAsJson
 
+import com.rallyhealth.vapors.v1.data.ExprState
 import io.circe.{Encoder, Json}
 
 package object circe extends CirceDsl with VaporsCirceEncoders {
 
   // TODO: Convert to implicit Encoder?
-  def serialize[PO : Encoder, O : Encoder](expr: ExprResult[PO, Nothing, O, Encoder]): Json = {
-    Json.fromJsonObject(expr.visit(InterpretExprResultAsJson.Visitor[PO]))
+  def serialize[PO, O](
+    expr: ExprResult[PO, Nothing, O, Encoder],
+  )(implicit
+    encodeState: Encoder[ExprState[PO, O]],
+  ): Json = {
+    Json.fromJsonObject(expr.visit(InterpretExprResultAsJson.Visitor(expr.state)))
   }
 }
