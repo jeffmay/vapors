10cc5d48fe82a58ca85bdc0bd747a5b08b032b88
Jeff May <jeff.n.may@gmail.com>
2021-10-05 18:19:25 -0700

    Replace DummyImplicit with NoOP

--- core-v1/src/main/scala/dsl/standard.scala
+++ core-v1/src/main/scala/dsl/standard.scala
@@ -6,11 +6,11 @@ import debug.HasSourceCodeInfo
 
 object standard extends BuildIdExprDsl with StandardRunDsl {
 
-  override type OP[_] = DummyImplicit
+  override type OP[_] = NoOP
 
   final object justified extends BuildJustifiedExprDsl with StandardRunDsl {
 
-    override type OP[_] = DummyImplicit
+    override type OP[_] = NoOP
   }
 
   final object withSourceInfo extends BuildIdExprDsl with StandardRunDsl {
