7337304475d2a6fa2f0fb559cf7836f1492d316e
Jeff May <jeff.n.may@gmail.com>
2021-10-22 17:55:50 -0700

    Fixed DebuggingToStringSpec

--- core-v1/src/test/scala/DebuggingToStringSpec.scala
+++ core-v1/src/test/scala/DebuggingToStringSpec.scala
@@ -1,6 +1,7 @@
 package com.rallyhealth.vapors.v1
 
-import com.rallyhealth.vapors.v1.debug.{Debugging, NoDebugging}
+import debug.{Debugging, NoDebugging}
+
 import munit.FunSuite
 
 class DebuggingToStringSpec extends FunSuite {
@@ -16,10 +17,6 @@ class DebuggingToStringSpec extends FunSuite {
 
   test("Debugging[Any, Option[Int]].toString") {
     val debuggingAnyOption = Debugging[Any, Option[Int]](_ => {})
-    // Unfortunately, this toString is not as nice as it could be if we used more advanced type tags
-    // However, requiring type tags for this toString method is probably overkill. If we decide to
-    // add more type information to the Debugging class for other purposes, we should also update
-    // the toString method and this test.
-    assertEquals(debuggingAnyOption.toString, "Debugging[Any, scala.Option]")
+    assertEquals(debuggingAnyOption.toString, "Debugging[Any, Option[+Int]]")
   }
 }
